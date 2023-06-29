d13ee098cf7d093da057cbc179fc85222ab8fd29
Phan Bao <anonymousid2411@gmail.com>
2023-06-30 00:49:58 +0700

    api get list room

--- server/controllers/room.controller.js
+++ server/controllers/room.controller.js
@@ -1,10 +1,6 @@
 const bcrypt = require("bcryptjs");
 const { pool } = require("../db");
 const jwt = require("jsonwebtoken");
-const UserProfile = require("../models/userprofile.model");
-const { USER_ROLE } = require("../consts/user.const");
-const { Op } = require("sequelize");
-const ApartmentModel = require("../models/apartment.model");
 const RoomModel = require("../models/room.model");
 
 const createRoom = async (req, res) => {
@@ -36,66 +32,31 @@ const createRoom = async (req, res) => {
         });
     }
 };
-const updateUser = async (req, res) => {
-    // Hash and salt password
 
+const updateRoom = async (req, res) => {
     try {
         const { id } = req.params;
-        const { username, phone, password, role: newRole } = req.body;
-        // validate data
-        if (!username || !phone) {
+        if (!id) {
+            res.json({
+                error: "Room is not found",
+            });
+            return;
+        }
+        const { name, address, status, cost, maxAllow } = req.body;
+        if (!name || !address || !status || !cost || !maxAllow) {
             res.json({
                 error: "Missing required fields",
             });
             return;
         }
-        if (username.length > 50) {
-            res.json({
-                error: "Username is too long",
-            });
-            return;
-        }
-        // if (password.length > 20) {
-        //     res.json({
-        //         error: "password is too long",
-        //     });
-        //     return;
-        // }
-        // if (password.length < 6) {
-        //     res.json({
-        //         error: "password is too short",
-        //     });
-        //     return;
-        // }
-        if (!phone.match(/^\d{10}$/)) {
-            res.json({
-                error: "Phone number is not in the correct format",
-            });
-            return;
-        }
 
-        const hasExistUser = await UserProfile.findAll({
-            where: {
-                phone,
-                id: {
-                    [Op.ne]: id,
-                },
-            },
-        });
-        if (hasExistUser.length > 0) {
-            res.json({
-                code: 1001,
-                error: "This phone number already exists",
-            });
-            return;
-        }
-
-        // const hasExistUser = await pool.query("SELECT * FROM userprofile WHERE userprofile.phone=$1", [phone]);
-        const currentUser = await UserProfile.update(
+        const currentRoom = await RoomModel.update(
             {
-                phone,
-                username,
-                role: newRole,
+                name,
+                address,
+                status,
+                cost,
+                maxAllow,
             },
             {
                 where: {
@@ -103,37 +64,9 @@ const updateUser = async (req, res) => {
                 },
             }
         );
-        if (!currentUser) {
-            res.json({
-                code: 1001,
-                error: "Current user is not exist",
-            });
-            return;
-        }
-        console.log("currentUser", currentUser);
-
-        // const result = await pool.query("INSERT INTO userprofile (username, phone, password, role) VALUES ($1, $2, $3, $4) RETURNING id", [
-        //     username,
-        //     phone,
-        //     hashedPassword,
-        //     0,
-        // ]);
-        // const result = await UserProfile.fi({
-        //     username,
-        //     phone,
-        //     password: hashedPassword,
-        //     role: newRole || USER_ROLE.Tenant,
-        // });
-        // const { id, role, username: resultUsername } = result;
-
-        // // Generate jwt token
-        // const token = jwt.sign(
-        //     { id, role, username: resultUsername },
-        //     "secret"
-        // );
 
         res.json({
-            data: currentUser,
+            data: currentRoom,
         });
     } catch (err) {
         console.error(err);
@@ -142,145 +75,21 @@ const updateUser = async (req, res) => {
         });
     }
 };
-const login = async (req, res) => {
-    const { phone, password } = req.body;
-    if (!phone.match(/^\d{10}$/)) {
-        res.json({
-            error: "Phone number is not in the correct format",
-        });
-        return;
-    }
-    // const result = await pool.query("SELECT * FROM userprofile WHERE userprofile.phone = $1", [phone]);
-    const user = await UserProfile.findOne({
-        where: {
-            phone,
-        },
-    });
-
-    const { id, role, username } = user || {};
-
-    if (!user) {
-        return res.json({
-            error: "Invalid phone",
-        });
-    }
-
-    // Verify password
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-        return res.json({
-            error: "Invalid password",
-        });
-    }
-
-    // Generate jwt token
-    const token = jwt.sign({ id, role, username }, "secret");
-
-    res.json({ data: { token, id, role, username, phone } });
-};
-const getUserById = async (req, res) => {
-    const { id } = req.params;
-    if (!id) {
-        res.json({
-            error: "Missing id",
-        });
-        return;
-    }
-    // const result = await pool.query("SELECT * FROM userprofile WHERE userprofile.id = $1", [id]);
-    const user = await UserProfile.findOne({
-        where: {
-            id,
-        },
-    });
-    user.password = undefined;
-    // const user = result.rows[0];
-
-    if (!user) {
-        return res.json({
-            error: "Current user not found",
-        });
-    }
-
-    res.json({
-        data: {
-            id: user.id,
-            username: user.username,
-            phone: user.phone,
-            role: user.role,
-        },
-    });
-};
-const getTenants = async (req, res) => {
-    const { id, role } = req.decoded;
-    if (role !== USER_ROLE.Admin) {
-        res.json({
-            error: "Don't have permission",
-        });
-        return;
-    }
-    if (!id) {
-        res.json({
-            error: "Missing id",
-        });
-        return;
-    }
-    // const result = await pool.query("SELECT * FROM userprofile WHERE userprofile.id = $1", [id]);
-    const users = await UserProfile.findAll({
-        attributes: ["id", "username", "phone", "role"],
-    });
-
-    // const user = result.rows[0];
-
-    if (!users) {
-        return res.json({
-            error: "Current user not found",
-        });
-    }
-
-    res.json({
-        items: users,
-    });
-};
-
-const getMe = async (req, res) => {
+const getListRoom = async (req, res) => {
     try {
-        const { id } = req.decoded;
-        if (!id) {
-            res.json({
-                error: "Missing id",
-            });
-            return;
-        }
-        // const result = await pool.query("SELECT * FROM userprofile WHERE userprofile.id = $1", [userId]);
-        // const user = result.rows[0];
-        const user = await UserProfile.findOne({
-            where: {
-                id,
-            },
-        });
-
-        if (!user) {
-            return res.json({
-                error: "Current user not found",
-            });
-        }
-
+        const roomList = await RoomModel.findAll();
         res.json({
-            data: {
-                id: user.id,
-                username: user.username,
-                phone: user.phone,
-                role: user.role,
-            },
+            data: roomList,
         });
-    } catch (error) {
-        res.json({
-            error: error,
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            error: err,
         });
     }
 };
-const deleteTenant = async (req, res) => {
+
+const deleteRoom = async (req, res) => {
     try {
         const { id: currentId } = req.decoded;
         const { id } = req.params;
@@ -290,23 +99,21 @@ const deleteTenant = async (req, res) => {
             });
             return;
         }
-        // const result = await pool.query("SELECT * FROM userprofile WHERE userprofile.id = $1", [userId]);
-        // const user = result.rows[0];
-        const user = await UserProfile.destroy({
+        const room = await RoomModel.destroy({
             where: {
                 id,
             },
         });
 
-        if (!user) {
+        if (!room) {
             return res.json({
-                error: "Current user not found",
+                error: "Current room not found",
             });
         }
 
         res.json({
             data: {
-                user,
+                room,
             },
         });
     } catch (error) {
@@ -318,10 +125,7 @@ const deleteTenant = async (req, res) => {
 
 module.exports = {
     createRoom,
-    deleteTenant,
-    login,
-    getUserById,
-    getMe,
-    getTenants,
-    updateUser,
+    updateRoom,
+    getListRoom,
+    deleteRoom,
 };
--- server/routers/room.route.js
+++ server/routers/room.route.js
@@ -1,7 +1,16 @@
 var express = require("express");
 var router = express.Router();
-const { createRoom } = require("../controllers/room.controller");
+const {
+    createRoom,
+    getListRoom,
+    updateRoom,
+    deleteRoom,
+} = require("../controllers/room.controller");
 const { isAuth } = require("../middlewares/auth.middleware");
 
 router.post("/create", isAuth, createRoom);
+router.post("/update/:id", isAuth, updateRoom);
+router.delete("/:id", isAuth, deleteRoom);
+router.get("/list", isAuth, getListRoom);
+
 module.exports = router;
